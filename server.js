57cb7d9561950a32ab87f16d067675f1cac1b40c
LluisManas <manastorra@gmail.com>
2020-05-05 12:01:46 +0200

    JWT

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const users = require("./routes/api/user");
+const config = require("config");
 
 const app = express();
 
@@ -17,6 +18,7 @@ mongoose
   .catch((err) => console.log(err));
 
 app.use("/api/user", users);
+app.use("/api/auth", require("./routes/api/auth"));
 
 const port = process.env.PORT || 5001;
 
